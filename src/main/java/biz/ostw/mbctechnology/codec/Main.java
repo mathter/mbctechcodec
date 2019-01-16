0fef4e96aac774463c5f6da2a005939583908570
mathter <mathter@mail.ru>
2019-01-16 23:28:01 +0700

    View of mqi files

--- src/main/java/biz/ostw/mbctechnology/codec/Main.java
+++ src/main/java/biz/ostw/mbctechnology/codec/Main.java
@@ -7,9 +7,6 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @FXML
-    private MenuBar menuBar;
-
     public static void main(String[] args) {
 
         launch(args);
